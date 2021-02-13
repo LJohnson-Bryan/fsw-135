b6b11aab4e2e0a90d8f17ce291a0f759ad7b02ac
LJohnson-Bryan <logan.johnson@bryanuniversity.edu>
2021-02-13 01:18:42 -0500

    Rock-Your-Vote pt. 2

--- week4/rock-the-vote/client/src/components/CreateIssue/index.js
+++ week4/rock-the-vote/client/src/components/CreateIssue/index.js
@@ -19,11 +19,11 @@ const CreateIssue = props => {
 
     return ( 
         <form onSubmit={submitIssue}>
-            <label for="issue" class="block text-sm font-medium text-gray-700">Describe your issue:</label>
-            <div class="mt-1">
-            <textarea name="issue" value={issueField} required onChange={e => setIssueField(e.target.value)} class="w-full min-h-30 px-3 py-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline" placeholder="The moon is just a circle in the sky painted by artists.."></textarea>
+            <label htmlFor="issue" className="block text-sm font-medium text-gray-700">Describe your issue:</label>
+            <div className="mt-1">
+            <textarea name="issue" value={issueField} required onChange={e => setIssueField(e.target.value)} className="w-full min-h-30 px-3 py-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline" placeholder="The moon is just a circle in the sky painted by artists.."></textarea>
             </div>
-            <button type="submit" class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <button type="submit" className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 Create issue
             </button>
         </form>
